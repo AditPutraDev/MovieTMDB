406f7c98f48e1500ee6c207eaf6cbf6e9786bf37
unknown <aditya_hermansyahputra@yahoo.com>
2019-08-28 20:10:33 +0700

    fix detail xml

--- app/src/main/java/com/dicoding/movie/activity/MDetailActivity.java
+++ app/src/main/java/com/dicoding/movie/activity/MDetailActivity.java
@@ -10,6 +10,8 @@ import com.dicoding.movie.R;
 import com.dicoding.movie.model.Movie;
 import com.squareup.picasso.Picasso;
 
+import jp.wasabeef.picasso.transformations.BlurTransformation;
+
 public class MDetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_MOVIE = "extra_movie";
@@ -24,11 +26,15 @@ public class MDetailActivity extends AppCompatActivity {
         if (movie != null) {
             TextView tvTitle = findViewById(R.id.tvTitle);
             TextView tvOverview = findViewById(R.id.tvOverview);
+            TextView tvRelease = findViewById(R.id.tvRelease);
             ImageView imgPoster = findViewById(R.id.imgPoster);
+            ImageView imgBackdrop = findViewById(R.id.item_backdrop);
 
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
+            tvRelease.setText(movie.getReleaseDate());
             Picasso.get().load(movie.getPosterPath()).into(imgPoster);
+            Picasso.get().load(movie.getBackdropPath()).transform(new BlurTransformation(getBaseContext(), 10)).into(imgBackdrop);
         }
     }
 }
--- app/src/main/java/com/dicoding/movie/activity/TvDetailActivity.java
+++ app/src/main/java/com/dicoding/movie/activity/TvDetailActivity.java
@@ -10,6 +10,8 @@ import com.dicoding.movie.R;
 import com.dicoding.movie.model.TvShow;
 import com.squareup.picasso.Picasso;
 
+import jp.wasabeef.picasso.transformations.BlurTransformation;
+
 public class TvDetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_TV = "extra_tvshow";
@@ -24,11 +26,15 @@ public class TvDetailActivity extends AppCompatActivity {
         if (tvShow != null) {
             TextView tvName = findViewById(R.id.tvName);
             TextView tvOverview = findViewById(R.id.tvOverviewTv);
+            TextView tvRelease = findViewById(R.id.tvReleaseTv);
             ImageView imgPoster = findViewById(R.id.imgPosterTv);
+            ImageView imgBackdrop = findViewById(R.id.item_backdropTv);
 
             tvName.setText(tvShow.getName());
             tvOverview.setText(tvShow.getOverview());
+            tvRelease.setText(tvShow.getFirstAirDate());
             Picasso.get().load(tvShow.getPosterPath()).into(imgPoster);
+            Picasso.get().load(tvShow.getBackdropPath()).transform(new BlurTransformation(getBaseContext(), 10)).into(imgBackdrop);
         }
     }
 }
